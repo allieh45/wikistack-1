710e10272c2fe116079d51a5a93b799ffc06f7a7
Allie Humphrey <alliehumphrey@yahoo.com>
2021-03-11 14:08:58 -0600

    set up basic routes

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const { db, Page, User } = require("./models");
+const wikiRouter = require("./routes/wiki");
+const userRouter = require("./routes/users");
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+app.use("/wiki", wikiRouter);
+app.use("/users", userRouter);
+
+app.get("/", (req, res, next) => {
+  res.send("Hello World");
+});
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+const init = async () => {
+  await db.sync({ force: true });
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+  });
+};
+
+init();
---
+++ routes/wiki.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const express = require("express");
+const wikiRouter = express.Router();
+
+wikiRouter.get("/", (req, res, next) => {
+  res.send("got to GET /wiki/");
+});
+
+wikiRouter.post("/", (req, res, next) => {
+  res.send("got to POST /wiki/");
+});
+
+wikiRouter.get("/add", (req, res, next) => {
+  res.send("got to GET /wiki/add");
+});
+
+module.exports = wikiRouter;
